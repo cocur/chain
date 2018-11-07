afbe6be6af651a0c83cfe1ebed1e1f9551e219df
Panupong Mongkoladisai <panupong.m@maqe.com>
2018-11-07 16:43:36 +0700

    fix slice() chain

--- tests/Link/SliceTest.php
+++ tests/Link/SliceTest.php
@@ -26,4 +26,17 @@ class SliceTest extends PHPUnit_Framework_TestCase
 
         $this->assertEquals([1, 2, 3], $mock->array);
     }
+
+    /**
+     * @test
+     * @covers Cocur\Chain\Link\Slice::slice()
+     */
+    public function sliceCanChain()
+    {
+        /** @var \Cocur\Chain\Link\Slice $mock */
+        $mock        = $this->getMockForTrait('Cocur\Chain\Link\Slice');
+        $mock->array = [0, 1, 2, 3, 4, 5];
+        $result      = $mock->slice(1, 3)->array;
+        $this->assertEquals([1, 2, 3], $result);
+    }
 }
